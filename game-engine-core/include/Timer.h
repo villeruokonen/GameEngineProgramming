adccdc0c79bd3c211cbff7c7a3d3b7b430655a2a
Ville Ruokonen <ruokonenv@gmail.com>
2024-09-11 16:12:13 +0300

    Create Timer

---
+++ game-engine-core/include/Timer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+class Timer
+{
+public:
+	Timer();
+
+	void BeginTimer();
+	void EndTimer();
+
+	inline float GetElapsedSeconds() const { return m_fElapsedSeconds; }
+
+	static uint64_t GetTicks();
+
+private:
+	double		m_dRateToSeconds;
+	uint64_t	m_uStartClock;
+	uint64_t	m_uTickFrequency;
+
+	float		m_fElapsedSeconds;
+};

792bb2354597d4628c0377cb424a37818728390f
Ville Ruokonen <ruokonenv@gmail.com>
2024-09-25 17:36:25 +0300

    Add OpenGL external methods, implement methods
    Loading textures, compile shader parts from code

--- game-engine-core/include/OpenGLRenderer.h
+++ game-engine-core/include/OpenGLRenderer.h
@@ -4,6 +4,8 @@
 #include "IApplication.h"
 
 #include <gl/GL.h>
+#include "./GL/glext.h"
+#include "./GL/myGL.h"
 
 class OpenGLRenderer : public IRenderer
 {
@@ -11,16 +13,63 @@ public:
 	OpenGLRenderer();
 	~OpenGLRenderer();
 
+	/**
+	 * Create (from IRenderer)
+	 * @return true if succesful, or false to cancel app startup
+	 */
 	bool Create() override;
+
+	/**
+	 * Flip (from IRenderer)
+	 * transfer graphics to screen
+	 */
 	void Flip() override;
-	void Clear(float r, float g, float b, float a, float depth, int32_t stencil) override;
+
+	/**
+	 * Clear
+	 * clear the color buffer, depth buffer and stencil
+	 */
+	void Clear(float r, float g, float b, float a, float depth = 1.0f, int32_t stencil = 0) override;
+
+	/**
+	 * SetViewport
+	 * set viewport area of the screen
+	 * @param area, x, y, width, height of the viewport area
+	 */
 	void SetViewport(const glm::ivec4& area) override;
-	bool SetTexture(uint32_t program, uint32_t texture, uint32_t slot, const std::string_view& uniformName) override;
+
+	/**
+	 * SetTexture
+	 * @param program
+	 * @param texture handle
+	 * @param slot index of the texture slot
+	 * @param uniformName name of the uniform in shader
+	 * @return true if uniform is found and set
+	 */
+	bool SetTexture(GLuint program, GLuint texture, uint32_t slot, const std::string_view& uniformName) override;
+
+	GLuint CreateTexture(const std::string_view& filename);
+
+	GLuint CreateVertexShader(const char* sourceCode);
+	GLuint CreateVertexShaderFromFile(const std::string_view& filename);
+
+	GLuint CreateFragmentShader(const char* sourceCode);
+	GLuint CreateFragmentShaderFromFile(const std::string_view& filename);
+
+	GLuint CreateProgram(GLuint vertexShader, GLuint fragmentShader);
+
+	void PrintShaderError(GLuint shader);
+	void PrintProgramError(GLuint program);
 
 private:
+	/**
+	 * InitFunctions
+	 * static helper to get OpenGL function pointers after context has been created
+	 */
+	static bool InitFunctions();
 	bool SetDefaultSettings();
 
-	HDC			m_Context;	// handle to device context 
-	HGLRC		m_Resource; // OpenGL handle to resource context 
-
+	HDC			m_Context;		// handle to device context
+	HGLRC		m_Resource;		// handle to resource context
 };
+
--- game-engine-core/src/OpenGLRenderer.cpp
+++ game-engine-core/src/OpenGLRenderer.cpp
@@ -1,5 +1,84 @@
 #include "../include/OpenGLRenderer.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/stb_image.h"
+
+PFNGLBLENDEQUATIONSEPARATEPROC glBlendEquationSeparate = nullptr;
+PFNGLBLENDFUNCSEPARATEPROC glBlendFuncSeparate = nullptr;
+
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+PFNGLUNIFORM2FPROC glUniform2f = nullptr;
+PFNGLUNIFORM3FPROC glUniform3f = nullptr;
+PFNGLUNIFORM4FPROC glUniform4f = nullptr;
+PFNGLDELETEBUFFERSPROC glDeleteBuffers = nullptr;
+PFNGLCREATEPROGRAMPROC glCreateProgram = nullptr;
+PFNGLDELETEPROGRAMPROC glDeleteProgram = nullptr;
+PFNGLUSEPROGRAMPROC glUseProgram = nullptr;
+PFNGLATTACHSHADERPROC glAttachShader = nullptr;
+PFNGLDETACHSHADERPROC glDetachShader = nullptr;
+PFNGLLINKPROGRAMPROC glLinkProgram = nullptr;
+PFNGLGETPROGRAMIVPROC glGetProgramiv = nullptr;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog = nullptr;
+PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = nullptr;
+PFNGLUNIFORM1IPROC glUniform1i = nullptr;
+PFNGLUNIFORM1IVPROC glUniform1iv = nullptr;
+PFNGLUNIFORM2IVPROC glUniform2iv = nullptr;
+PFNGLUNIFORM3IVPROC glUniform3iv = nullptr;
+PFNGLUNIFORM4IVPROC glUniform4iv = nullptr;
+PFNGLUNIFORM1FPROC glUniform1f = nullptr;
+PFNGLUNIFORM1FVPROC glUniform1fv = nullptr;
+PFNGLUNIFORM2FVPROC glUniform2fv = nullptr;
+PFNGLUNIFORM3FVPROC glUniform3fv = nullptr;
+PFNGLUNIFORM4FVPROC glUniform4fv = nullptr;
+PFNGLUNIFORMMATRIX3FVPROC glUniformMatrix3fv = nullptr;
+PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv = nullptr;
+PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation = nullptr;
+PFNGLVERTEXATTRIB1FPROC glVertexAttrib1f = nullptr;
+PFNGLVERTEXATTRIB1FVPROC glVertexAttrib1fv = nullptr;
+PFNGLVERTEXATTRIB2FVPROC glVertexAttrib2fv = nullptr;
+PFNGLVERTEXATTRIB3FVPROC glVertexAttrib3fv = nullptr;
+PFNGLVERTEXATTRIB4FVPROC glVertexAttrib4fv = nullptr;
+PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray = nullptr;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray = nullptr;
+PFNGLBINDATTRIBLOCATIONPROC glBindAttribLocation = nullptr;
+PFNGLGETACTIVEUNIFORMPROC glGetActiveUniform = nullptr;
+
+// Shader
+PFNGLCREATESHADERPROC glCreateShader = nullptr;
+PFNGLDELETESHADERPROC glDeleteShader = nullptr;
+PFNGLSHADERSOURCEPROC glShaderSource = nullptr;
+PFNGLCOMPILESHADERPROC glCompileShader = nullptr;
+PFNGLGETSHADERIVPROC glGetShaderiv = nullptr;
+
+// VBO
+PFNGLGENBUFFERSPROC glGenBuffers = nullptr;
+PFNGLBINDBUFFERPROC	glBindBuffer = nullptr;
+PFNGLBUFFERDATAPROC	glBufferData = nullptr;
+PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer = nullptr;
+PFNGLVERTEXATTRIBIPOINTERPROC glVertexAttribIPointer = nullptr;
+
+
+PFNGLGENFRAMEBUFFERSPROC glGenFramebuffers = nullptr;
+PFNGLGENRENDERBUFFERSPROC glGenRenderbuffers = nullptr;
+PFNGLDELETEFRAMEBUFFERSPROC glDeleteFramebuffers = nullptr;
+PFNGLDELETERENDERBUFFERSPROC glDeleteRenderbuffers = nullptr;
+PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer = nullptr;
+PFNGLBINDRENDERBUFFERPROC glBindRenderbuffer = nullptr;
+PFNGLRENDERBUFFERSTORAGEPROC glRenderbufferStorage = nullptr;
+PFNGLFRAMEBUFFERTEXTURE2DPROC glFramebufferTexture2D = nullptr;
+PFNGLFRAMEBUFFERTEXTUREPROC glFramebufferTexture = nullptr;
+PFNGLFRAMEBUFFERRENDERBUFFERPROC glFramebufferRenderbuffer = nullptr;
+PFNGLCHECKFRAMEBUFFERSTATUSPROC glCheckFramebufferStatus = nullptr;
+PFNGLCLEARDEPTHFPROC glClearDepthf = nullptr;
+PFNGLGENERATEMIPMAPPROC glGenerateMipmap = nullptr;
+
+#include "../include/GL/wglext.h"
+PFNGLBLENDEQUATIONPROC glBlendEquation = nullptr;
+PFNGLACTIVETEXTUREPROC glActiveTexture = nullptr;
+PFNGLCOMPRESSEDTEXIMAGE2D glCompressedTexImage2D = nullptr;
+PFNGLBLENDCOLORPROC glBlendColor = nullptr;
+PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
+
 OpenGLRenderer::OpenGLRenderer() :
 	m_Context(nullptr),
 	m_Resource(nullptr)
@@ -20,48 +99,44 @@ OpenGLRenderer::~OpenGLRenderer()
 bool OpenGLRenderer::Create()
 {
 	m_Context = ::GetDC(IApplication::GetApp()->GetWindow());
-	uint32_t flags = PFD_DRAW_TO_WINDOW
-		| PFD_SUPPORT_OPENGL
-		| PFD_DOUBLEBUFFER;
+	uint32_t flags = PFD_DRAW_TO_WINDOW |
+		PFD_SUPPORT_OPENGL |
+		PFD_DOUBLEBUFFER;
 
 	PIXELFORMATDESCRIPTOR pfd =
 	{
-		sizeof(PIXELFORMATDESCRIPTOR),	// size of type (C bullshit)
-		1,								// version
+		sizeof(PIXELFORMATDESCRIPTOR),
+		1,
 		flags,
-		PFD_TYPE_RGBA,					// frame buffer pixel type: RGBA or palette (color index)?
-		32,								// color depth of frame buffer
-		0, 0, 0, 0, 0, 0,				// RGB bits / shift
-		0,								// alpha bits
-		0,								// alpha shift
-		0,								// accum bits
-		0, 0, 0, 0,						// RGBA accum bits
-		24,								// depth buffer bits
-		8,								// stencil buffer bits
-		0,								// aux buffers
-		PFD_MAIN_PLANE,					// layer type
-		0,								// is reserved?
-		0, 0, 0							// masks						
+		PFD_TYPE_RGBA,	// kind of framebuffer, RGBA or palette.
+		32,				// colordepth of the framebuffer
+		0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		0,
+		0, 0, 0, 0,
+		24,				// depth buffer bits
+		8,				// stencil buffer bits
+		0,
+		PFD_MAIN_PLANE,
+		0,
+		0, 0, 0
 	};
 
-	// pixel format index
 	int pixelFormat = ::ChoosePixelFormat(m_Context, &pfd);
-
 	if (pixelFormat == 0)
 	{
-		// No compatible pixel format.
 		return false;
 	}
-
-	if(!::SetPixelFormat(m_Context, pixelFormat, &pfd))
+	if (!::SetPixelFormat(m_Context, pixelFormat, &pfd))
 	{
-		// Failed to set pixel format.
 		return false;
 	}
 
-	// Create and activate rendering context
 	m_Resource = wglCreateContext(m_Context);
 	wglMakeCurrent(m_Context, m_Resource);
+
+	InitFunctions();
 
 	SetDefaultSettings();
 
@@ -71,19 +146,15 @@ bool OpenGLRenderer::Create()
 void OpenGLRenderer::Flip()
 {
 	glFlush();
-
 	SwapBuffers(m_Context);
 }
 
 void OpenGLRenderer::Clear(float r, float g, float b, float a, float depth, int32_t stencil)
 {
-	// Set up what to clear
-	// TODO: glClearDepthf(depth);
+	glClearDepthf(depth);
 	glClearStencil(stencil);
 
 	glClearColor(r, g, b, a);
-
-	// Actually perform clear
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 }
 
@@ -94,8 +165,186 @@ void OpenGLRenderer::SetViewport(const glm::ivec4& area)
 
 bool OpenGLRenderer::SetTexture(uint32_t program, uint32_t texture, uint32_t slot, const std::string_view& uniformName)
 {
-	// TODO
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	const GLint location = glGetUniformLocation(program, uniformName.data());
+	if (location >= 0)
+	{
+		glUniform1i(location, slot);
+		return true;
+	}
+
 	return false;
+}
+
+GLuint OpenGLRenderer::CreateTexture(const std::string_view& filename)
+{
+	GLuint textureHandle = 0;
+	int32_t width = 0;
+	int32_t height = 0;
+	int32_t bitsPerPixel = 0;
+	uint8_t* imgData = stbi_load(filename.data(), &width, &height, &bitsPerPixel, STBI_rgb_alpha);
+
+	if (!imgData || !width || height || bitsPerPixel)
+	{
+		// failed to load image file
+		IApplication::Debug("Failed to load image");
+		IApplication::Debug(filename.data());
+		return 0;
+	}
+
+	// premultiply alpha for faster blending
+	const int32_t imgDataBytes = width * height * 4;
+	for (int32_t i = 0; i < imgDataBytes; i += 4)
+	{
+		const int32_t alpha = imgData[i + 3];
+		if (alpha != 255)
+		{
+			imgData[i] = imgData[i] * alpha / 255;
+			imgData[i + 1] = imgData[i + 1] * alpha / 255;
+			imgData[i + 2] = imgData[i + 2] * alpha / 255;
+		}
+	}
+
+	GLint internalFormat = GL_RGBA;
+	GLenum format = GL_RGBA;
+	GLenum err = glGetError(); // clear error buffer
+
+	glGenTextures(1, &textureHandle);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textureHandle);
+
+	glTexImage2D(GL_TEXTURE_2D,
+		0,
+		internalFormat,
+		width,
+		height,
+		0,
+		format,
+		GL_UNSIGNED_BYTE,
+		imgData);
+
+	delete[] imgData; // delete pixel block
+
+	err = glGetError();
+
+	return textureHandle;
+}
+
+GLuint OpenGLRenderer::CreateVertexShader(const char* sourceCode)
+{
+	GLuint shaderHandle = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(shaderHandle, 1, (const char**)&sourceCode, nullptr);
+	glCompileShader(shaderHandle);
+
+	// check if compilation successful
+	GLint shaderCompiled = 0;
+	glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &shaderCompiled);
+
+	if(!shaderCompiled)
+	{
+		IApplication::Debug("Failed to compile vertex shader: ");
+		PrintShaderError(shaderHandle);
+		glDeleteShader(shaderHandle);
+
+		shaderHandle = 0;
+	}
+
+	return shaderHandle;
+}
+
+GLuint OpenGLRenderer::CreateVertexShaderFromFile(const std::string_view& filename)
+{
+	std::ifstream f(filename.data(), std::ios::binary);
+	std::vector<char> bytes(
+		(std::istreambuf_iterator<char>(f)),
+		(std::istreambuf_iterator<char>()));
+
+	bytes.push_back(0); // add null terminator in case it is not there
+
+	return CreateVertexShader(bytes.data());
+}
+
+GLuint OpenGLRenderer::CreateFragmentShader(const char* sourceCode)
+{
+	GLuint shaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(shaderHandle, 1, (const char**)&sourceCode, nullptr);
+	glCompileShader(shaderHandle);
+
+	// check if compilation successful
+	GLint shaderCompiled = 0;
+	glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &shaderCompiled);
+
+	if (!shaderCompiled)
+	{
+		IApplication::Debug("Failed to compile fragment shader: ");
+		PrintShaderError(shaderHandle);
+		glDeleteShader(shaderHandle);
+
+		shaderHandle = 0;
+	}
+
+	return shaderHandle;
+}
+
+GLuint OpenGLRenderer::CreateFragmentShaderFromFile(const std::string_view& filename)
+{
+	std::ifstream f(filename.data(), std::ios::binary);
+	std::vector<char> bytes(
+		(std::istreambuf_iterator<char>(f)),
+		(std::istreambuf_iterator<char>()));
+
+	bytes.push_back(0); // add null terminator in case it is not there
+
+	return CreateFragmentShader(bytes.data());
+}
+
+GLuint OpenGLRenderer::CreateProgram(GLuint vertexShader, GLuint fragmentShader)
+{
+	return GLuint();
+}
+
+void OpenGLRenderer::PrintShaderError(GLuint shader)
+{
+	GLint infoLogLength = 0;
+	GLint charsWritten = 0;
+
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+	if (infoLogLength)
+	{
+		char* infoLog = new char[infoLogLength + 1];
+		memset(infoLog, 0, infoLogLength + 1);
+
+		glGetShaderInfoLog(shader, infoLogLength, &charsWritten, infoLog);
+
+		IApplication::Debug(infoLog);
+
+		delete[] infoLog;
+	}
+}
+
+void OpenGLRenderer::PrintProgramError(GLuint program)
+{
+	GLint infoLogLength = 0;
+	GLint charsWritten = 0;
+
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+	if (infoLogLength)
+	{
+		char* infoLog = new char[infoLogLength + 1];
+		memset(infoLog, 0, infoLogLength + 1);
+
+		glGetProgramInfoLog(program, infoLogLength, & charsWritten, infoLog);
+
+		IApplication::Debug(infoLog);
+
+		delete[] infoLog;
+	}
 }
 
 bool OpenGLRenderer::SetDefaultSettings()
@@ -103,8 +352,8 @@ bool OpenGLRenderer::SetDefaultSettings()
 	glStencilMask(0);
 	glDisable(GL_SCISSOR_TEST);
 
-	// TODO: glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// TODO: glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glPixelStorei(GL_PACK_ALIGNMENT, 1);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -113,15 +362,14 @@ bool OpenGLRenderer::SetDefaultSettings()
 	glEnable(GL_DEPTH_FUNC);
 	glDepthMask(GL_TRUE);
 	glDepthFunc(GL_LEQUAL);
-
 	glDisable(GL_STENCIL_TEST);
 
-	// TODO: glClearDepthf(1.0f);
+	glClearDepthf(1.0f);
 	glClearStencil(0);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-	// TODO: glBlendColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glBlendColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glEnable(GL_CULL_FACE);
 	glFrontFace(GL_CW);
@@ -129,3 +377,106 @@ bool OpenGLRenderer::SetDefaultSettings()
 
 	return true;
 }
+
+bool OpenGLRenderer::InitFunctions()
+{
+#if defined (_WINDOWS)
+#define GL_GETPROCADDRESS wglGetProcAddress
+#define GL_GETPROCADDRESS_PARAM_TYPE const char*
+#endif
+#if defined (_LINUX)
+#define GL_GETPROCADDRESS glXGetProcAddressARB
+#define GL_GETPROCADDRESS_PARAM_TYPE const GLubyte*
+#endif
+
+#if defined (_WINDOWS)
+	glBlendEquation = (PFNGLBLENDEQUATIONPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBlendEquation");
+	glActiveTexture = (PFNGLACTIVETEXTUREPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glActiveTexture");
+	glCompressedTexImage2D = (PFNGLCOMPRESSEDTEXIMAGE2D)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glCompressedTexImage2D");
+	glBlendColor = (PFNGLBLENDCOLORPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBlendColor");
+	wglChoosePixelFormatARB = (PFNWGLCHOOSEPIXELFORMATARBPROC)wglGetProcAddress("wglChoosePixelFormatARB");
+#endif
+
+	glBlendEquationSeparate = (PFNGLBLENDEQUATIONSEPARATEPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBlendEquationSeparate");
+	glBlendFuncSeparate = (PFNGLBLENDFUNCSEPARATEPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBlendFuncSeparate");
+
+	glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetProgramInfoLog");
+
+	glUniform2f = (PFNGLUNIFORM2FPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform2f");
+	glUniform3f = (PFNGLUNIFORM3FPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform3f");
+	glUniform4f = (PFNGLUNIFORM4FPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform4f");
+
+	glDeleteBuffers = (PFNGLDELETEBUFFERSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDeleteBuffers");
+
+	glBindBuffer = (PFNGLBINDBUFFERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBindBuffer");
+	glCreateProgram = (PFNGLCREATEPROGRAMPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glCreateProgram");
+	glDeleteProgram = (PFNGLDELETEPROGRAMPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDeleteProgram");
+	glUseProgram = (PFNGLUSEPROGRAMPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUseProgram");
+	glAttachShader = (PFNGLATTACHSHADERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glAttachShader");
+	glDetachShader = (PFNGLDETACHSHADERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDetachShader");
+	glLinkProgram = (PFNGLLINKPROGRAMPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glLinkProgram");
+	glGetProgramiv = (PFNGLGETPROGRAMIVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetProgramiv");
+	glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetShaderInfoLog");
+	glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetUniformLocation");
+	glUniform1i = (PFNGLUNIFORM1IPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform1i");
+	glUniform1iv = (PFNGLUNIFORM1IVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform1iv");
+	glUniform2iv = (PFNGLUNIFORM2IVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform2iv");
+	glUniform3iv = (PFNGLUNIFORM3IVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform3iv");
+	glUniform4iv = (PFNGLUNIFORM4IVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform4iv");
+	glUniform1f = (PFNGLUNIFORM1FPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform1f");
+	glUniform1fv = (PFNGLUNIFORM1FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform1fv");
+	glUniform2fv = (PFNGLUNIFORM2FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform2fv");
+	glUniform3fv = (PFNGLUNIFORM3FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform3fv");
+	glUniform4fv = (PFNGLUNIFORM4FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniform4fv");
+	glUniformMatrix3fv = (PFNGLUNIFORMMATRIX3FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniformMatrix3fv");
+	glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glUniformMatrix4fv");
+	glGetAttribLocation = (PFNGLGETATTRIBLOCATIONPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetAttribLocation");
+	glVertexAttrib1f = (PFNGLVERTEXATTRIB1FPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttrib1f");
+	glVertexAttrib1fv = (PFNGLVERTEXATTRIB1FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttrib1fv");
+	glVertexAttrib2fv = (PFNGLVERTEXATTRIB2FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttrib2fv");
+	glVertexAttrib3fv = (PFNGLVERTEXATTRIB3FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttrib3fv");
+	glVertexAttrib4fv = (PFNGLVERTEXATTRIB4FVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttrib4fv");
+	glEnableVertexAttribArray = (PFNGLDISABLEVERTEXATTRIBARRAYPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glEnableVertexAttribArray");
+	glDisableVertexAttribArray = (PFNGLDISABLEVERTEXATTRIBARRAYPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDisableVertexAttribArray");
+	glBindAttribLocation = (PFNGLBINDATTRIBLOCATIONPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBindAttribLocation");
+	glGetActiveUniform = (PFNGLGETACTIVEUNIFORMPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetActiveUniform");
+
+	// Shader
+	glCreateShader = (PFNGLCREATESHADERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glCreateShader");
+	glDeleteShader = (PFNGLDELETESHADERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDeleteShader");
+	glShaderSource = (PFNGLSHADERSOURCEPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glShaderSource");
+	glCompileShader = (PFNGLCOMPILESHADERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glCompileShader");
+	glGetShaderiv = (PFNGLGETSHADERIVPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGetShaderiv");
+
+	// VBO
+
+	glGenBuffers = (PFNGLGENBUFFERSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGenBuffers");
+	glBufferData = (PFNGLBUFFERDATAPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBufferData");
+	glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttribPointer");
+	glVertexAttribIPointer = (PFNGLVERTEXATTRIBIPOINTERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glVertexAttribIPointer");
+
+
+	glGenFramebuffers = (PFNGLGENFRAMEBUFFERSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGenFramebuffers");
+	glGenRenderbuffers = (PFNGLGENRENDERBUFFERSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGenRenderbuffers");
+	glDeleteFramebuffers = (PFNGLDELETEFRAMEBUFFERSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDeleteFramebuffers");
+	glDeleteRenderbuffers = (PFNGLDELETERENDERBUFFERSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glDeleteRenderbuffers");
+	glBindFramebuffer = (PFNGLBINDFRAMEBUFFERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBindFramebuffer");
+	glBindRenderbuffer = (PFNGLBINDRENDERBUFFERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glBindRenderbuffer");
+	glRenderbufferStorage = (PFNGLRENDERBUFFERSTORAGEPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glRenderbufferStorage");
+	glFramebufferTexture2D = (PFNGLFRAMEBUFFERTEXTURE2DPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glFramebufferTexture2D");
+	glFramebufferTexture = (PFNGLFRAMEBUFFERTEXTUREPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glFramebufferTexture");
+	glFramebufferRenderbuffer = (PFNGLFRAMEBUFFERRENDERBUFFERPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glFramebufferRenderbuffer");
+	glCheckFramebufferStatus = (PFNGLCHECKFRAMEBUFFERSTATUSPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glCheckFramebufferStatus");
+	glClearDepthf = (PFNGLCLEARDEPTHFPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glClearDepthf");
+	glGenerateMipmap = (PFNGLGENERATEMIPMAPPROC)GL_GETPROCADDRESS((GL_GETPROCADDRESS_PARAM_TYPE)"glGenerateMipmap");
+
+	// check that functions were loaded properly
+	if (!glCreateProgram)
+	{
+		IApplication::Debug("Renderer_OpenGL::InitFunctions - failed to find required OpenGL functions. Most likely there is no valid OpenGL drivers installed");
+		return false;
+	}
+
+	return true;
+}
+
